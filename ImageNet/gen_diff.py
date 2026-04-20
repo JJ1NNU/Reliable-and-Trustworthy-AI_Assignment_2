2e8617326aa17c95ec3360a42de3caa2130e460c
Jinwoo An <jinwo1111@gmail.com>
2026-04-21 00:45:19 +0900

    모델 3개 -> 2개 수정, 데이터 전처리 수정

--- ImageNet/gen_diff.py
+++ ImageNet/gen_diff.py
@@ -1,163 +1,118 @@
 '''
 usage: python gen_diff.py -h
 '''
-
 from __future__ import print_function
-
 import argparse
-
-from keras.applications.resnet50 import ResNet50
-from keras.applications.vgg16 import VGG16
-from keras.applications.vgg19 import VGG19
 from keras.layers import Input
-import scipy.misc
+from keras.models import load_model
+import numpy as np
+import random
+import os
+
+# Scipy.misc.imsave 대체 (최신 버전 호환성)
+try:
+    from imageio import imwrite
+except ImportError:
+    import scipy.misc
+    imwrite = scipy.misc.imsave
 
 from configs import bcolors
 from utils import *
 
-# read the parameter
-# argument parsing
-parser = argparse.ArgumentParser(
-    description='Main function for difference-inducing input generation in ImageNet dataset')
+# 1. Argument Parsing
+parser = argparse.ArgumentParser(description='DeepXplore for CIFAR-10 ResNet50')
 parser.add_argument('transformation', help="realistic transformation type", choices=['light', 'occl', 'blackout'])
-parser.add_argument('weight_diff', help="weight hyperparm to control differential behavior", type=float)
-parser.add_argument('weight_nc', help="weight hyperparm to control neuron coverage", type=float)
+parser.add_argument('weight_diff', help="weight hyperparm for differential behavior", type=float)
+parser.add_argument('weight_nc', help="weight hyperparm for neuron coverage", type=float)
 parser.add_argument('step', help="step size of gradient descent", type=float)
 parser.add_argument('seeds', help="number of seeds of input", type=int)
-parser.add_argument('grad_iterations', help="number of iterations of gradient descent", type=int)
-parser.add_argument('threshold', help="threshold for determining neuron activated", type=float)
-parser.add_argument('-t', '--target_model', help="target model that we want it predicts differently",
-                    choices=[0, 1, 2], default=0, type=int)
-parser.add_argument('-sp', '--start_point', help="occlusion upper left corner coordinate", default=(0, 0), type=tuple)
+parser.add_argument('grad_iterations', help="number of iterations", type=int)
+parser.add_argument('threshold', help="threshold for neuron activated", type=float)
+parser.add_argument('-t', '--target_model', help="target model index", choices=[0, 1], default=0, type=int)
+parser.add_argument('-sp', '--start_point', help="occlusion coordinate", default=(0, 0), type=tuple)
 parser.add_argument('-occl_size', '--occlusion_size', help="occlusion size", default=(50, 50), type=tuple)
 
 args = parser.parse_args()
 
-# input image dimensions
-img_rows, img_cols = 224, 224
+# 2. Setup
+img_rows, img_cols = 224, 224 # 학습 시 224로 리사이즈했다면 유지
 input_shape = (img_rows, img_cols, 3)
-
-# define input tensor as a placeholder
 input_tensor = Input(shape=input_shape)
 
-# load multiple models sharing same input tensor
+# 3. Load Models
 K.set_learning_phase(0)
-model1 = VGG16(input_tensor=input_tensor)
-model2 = VGG19(input_tensor=input_tensor)
-model3 = ResNet50(input_tensor=input_tensor)
-# init coverage table
-model_layer_dict1, model_layer_dict2, model_layer_dict3 = init_coverage_tables(model1, model2, model3)
+model1 = load_model('./models/resnet50_1.h5')
+model2 = load_model('./models/resnet50_2.h5')
 
-# ==============================================================================================
-# start gen inputs
-img_paths = image.list_pictures('./seeds/', ext='jpeg')
-for _ in xrange(args.seeds):
+# 레이어 이름 자동 탐색 (보통 마지막 층은 'dense_1' 혹은 'predictions'임)
+# 직접 확인하려면 model1.summary() 후 마지막 층 이름을 넣으세요.
+final_layer_name = model1.layers[-1].name 
+
+# 4. Init Coverage Table (2개 모델 위주로 처리)
+model_layer_dict1, model_layer_dict2, _ = init_coverage_tables(model1, model2, model2)
+
+# 결과 저장 폴더 생성
+if not os.path.exists('./generated_inputs/'):
+    os.makedirs('./generated_inputs/')
+
+# 5. Start Generation
+img_paths = image.list_pictures('./seeds/', ext='png') # .png 파일 읽기
+
+for _ in range(args.seeds):
     gen_img = preprocess_image(random.choice(img_paths))
     orig_img = gen_img.copy()
-    # first check if input already induces differences
-    pred1, pred2, pred3 = model1.predict(gen_img), model2.predict(gen_img), model3.predict(gen_img)
-    label1, label2, label3 = np.argmax(pred1[0]), np.argmax(pred2[0]), np.argmax(pred3[0])
-    if not label1 == label2 == label3:
-        print(bcolors.OKGREEN + 'input already causes different outputs: {}, {}, {}'.format(decode_label(pred1),
-                                                                                            decode_label(pred2),
-                                                                                            decode_label(
-                                                                                                pred3)) + bcolors.ENDC)
-
+    
+    pred1, pred2 = model1.predict(gen_img), model2.predict(gen_img)
+    label1, label2 = np.argmax(pred1[0]), np.argmax(pred2[0])
+    
+    if not label1 == label2:
+        print(bcolors.OKGREEN + 'Difference found! Model1: {}, Model2: {}'.format(label1, label2) + bcolors.ENDC)
         update_coverage(gen_img, model1, model_layer_dict1, args.threshold)
         update_coverage(gen_img, model2, model_layer_dict2, args.threshold)
-        update_coverage(gen_img, model3, model_layer_dict3, args.threshold)
-
-        print(bcolors.OKGREEN + 'covered neurons percentage %d neurons %.3f, %d neurons %.3f, %d neurons %.3f'
-              % (len(model_layer_dict1), neuron_covered(model_layer_dict1)[2], len(model_layer_dict2),
-                 neuron_covered(model_layer_dict2)[2], len(model_layer_dict3),
-                 neuron_covered(model_layer_dict3)[2]) + bcolors.ENDC)
-        averaged_nc = (neuron_covered(model_layer_dict1)[0] + neuron_covered(model_layer_dict2)[0] +
-                       neuron_covered(model_layer_dict3)[0]) / float(
-            neuron_covered(model_layer_dict1)[1] + neuron_covered(model_layer_dict2)[1] +
-            neuron_covered(model_layer_dict3)[
-                1])
-        print(bcolors.OKGREEN + 'averaged covered neurons %.3f' % averaged_nc + bcolors.ENDC)
-
+        
         gen_img_deprocessed = deprocess_image(gen_img)
-
-        # save the result to disk
-        scipy.misc.imsave('./generated_inputs/' + 'already_differ_' + decode_label(pred1) + '_' + decode_label(
-            pred2) + '_' + decode_label(pred3) + '.png', gen_img_deprocessed)
+        imwrite('./generated_inputs/already_differ_{}_{}.png'.format(label1, label2), gen_img_deprocessed)
         continue
 
-    # if all label agrees
+    # Joint Loss Function 구성
     orig_label = label1
     layer_name1, index1 = neuron_to_cover(model_layer_dict1)
     layer_name2, index2 = neuron_to_cover(model_layer_dict2)
-    layer_name3, index3 = neuron_to_cover(model_layer_dict3)
 
-    # construct joint loss function
+    # Differential behavior loss
     if args.target_model == 0:
-        loss1 = -args.weight_diff * K.mean(model1.get_layer('predictions').output[..., orig_label])
-        loss2 = K.mean(model2.get_layer('predictions').output[..., orig_label])
-        loss3 = K.mean(model3.get_layer('fc1000').output[..., orig_label])
-    elif args.target_model == 1:
-        loss1 = K.mean(model1.get_layer('predictions').output[..., orig_label])
-        loss2 = -args.weight_diff * K.mean(model2.get_layer('predictions').output[..., orig_label])
-        loss3 = K.mean(model3.get_layer('fc1000').output[..., orig_label])
-    elif args.target_model == 2:
-        loss1 = K.mean(model1.get_layer('predictions').output[..., label1])
-        loss2 = K.mean(model2.get_layer('predictions').output[..., orig_label])
-        loss3 = -args.weight_diff * K.mean(model3.get_layer('fc1000').output[..., orig_label])
-    loss1_neuron = K.mean(model1.get_layer(layer_name1).output[..., index1])
-    loss2_neuron = K.mean(model2.get_layer(layer_name2).output[..., index2])
-    loss3_neuron = K.mean(model3.get_layer(layer_name3).output[..., index3])
-    layer_output = (loss1 + loss2 + loss3) + args.weight_nc * (loss1_neuron + loss2_neuron + loss3_neuron)
+        loss_diff = -args.weight_diff * K.mean(model1.output[..., orig_label]) + K.mean(model2.output[..., orig_label])
+    else:
+        loss_diff = K.mean(model1.output[..., orig_label]) - args.weight_diff * K.mean(model2.output[..., orig_label])
 
-    # for adversarial image generation
-    final_loss = K.mean(layer_output)
-
-    # we compute the gradient of the input picture wrt this loss
+    # Neuron coverage loss
+    loss_nc = args.weight_nc * (K.mean(model1.get_layer(layer_name1).output[..., index1]) + 
+                                K.mean(model2.get_layer(layer_name2).output[..., index2]))
+    
+    final_loss = K.mean(loss_diff + loss_nc)
     grads = normalize(K.gradients(final_loss, input_tensor)[0])
+    iterate = K.function([input_tensor], [grads])
 
-    # this function returns the loss and grads given the input picture
-    iterate = K.function([input_tensor], [loss1, loss2, loss3, loss1_neuron, loss2_neuron, loss3_neuron, grads])
-
-    # we run gradient ascent for 20 steps
-    for iters in xrange(args.grad_iterations):
-        loss_value1, loss_value2, loss_value3, loss_neuron1, loss_neuron2, loss_neuron3, grads_value = iterate(
-            [gen_img])
+    # Gradient Descent (Ascent)
+    for iters in range(args.grad_iterations):
+        grads_value = iterate([gen_img])[0]
         if args.transformation == 'light':
-            grads_value = constraint_light(grads_value)  # constraint the gradients value
+            grads_value = constraint_light(grads_value)
         elif args.transformation == 'occl':
-            grads_value = constraint_occl(grads_value, args.start_point,
-                                          args.occlusion_size)  # constraint the gradients value
-        elif args.transformation == 'blackout':
-            grads_value = constraint_black(grads_value)  # constraint the gradients value
+            grads_value = constraint_occl(grads_value, args.start_point, args.occlusion_size)
 
         gen_img += grads_value * args.step
-        pred1, pred2, pred3 = model1.predict(gen_img), model2.predict(gen_img), model3.predict(gen_img)
-        label1, label2, label3 = np.argmax(pred1[0]), np.argmax(pred2[0]), np.argmax(pred3[0])
+        pred1, pred2 = model1.predict(gen_img), model2.predict(gen_img)
+        label1, label2 = np.argmax(pred1[0]), np.argmax(pred2[0])
 
-        if not label1 == label2 == label3:
+        if not label1 == label2:
             update_coverage(gen_img, model1, model_layer_dict1, args.threshold)
             update_coverage(gen_img, model2, model_layer_dict2, args.threshold)
-            update_coverage(gen_img, model3, model_layer_dict3, args.threshold)
-
-            print(bcolors.OKGREEN + 'covered neurons percentage %d neurons %.3f, %d neurons %.3f, %d neurons %.3f'
-                  % (len(model_layer_dict1), neuron_covered(model_layer_dict1)[2], len(model_layer_dict2),
-                     neuron_covered(model_layer_dict2)[2], len(model_layer_dict3),
-                     neuron_covered(model_layer_dict3)[2]) + bcolors.ENDC)
-            averaged_nc = (neuron_covered(model_layer_dict1)[0] + neuron_covered(model_layer_dict2)[0] +
-                           neuron_covered(model_layer_dict3)[0]) / float(
-                neuron_covered(model_layer_dict1)[1] + neuron_covered(model_layer_dict2)[1] +
-                neuron_covered(model_layer_dict3)[
-                    1])
-            print(bcolors.OKGREEN + 'averaged covered neurons %.3f' % averaged_nc + bcolors.ENDC)
+            
+            nc1 = neuron_covered(model_layer_dict1)[2]
+            nc2 = neuron_covered(model_layer_dict2)[2]
+            print(bcolors.OKGREEN + 'NC: Model1: {:.3f}, Model2: {:.3f}'.format(nc1, nc2) + bcolors.ENDC)
 
             gen_img_deprocessed = deprocess_image(gen_img)
-            orig_img_deprocessed = deprocess_image(orig_img)
-
-            # save the result to disk
-            scipy.misc.imsave(
-                './generated_inputs/' + args.transformation + '_' + decode_label(pred1) + '_' + decode_label(
-                    pred2) + '_' + decode_label(pred3) + '.png', gen_img_deprocessed)
-            scipy.misc.imsave(
-                './generated_inputs/' + args.transformation + '_' + decode_label(pred1) + '_' + decode_label(
-                    pred2) + '_' + decode_label(pred3) + '_orig.png', orig_img_deprocessed)
+            imwrite('./generated_inputs/{}_{}_{}.png'.format(args.transformation, label1, label2), gen_img_deprocessed)
             break
--- ImageNet/utils.py
+++ ImageNet/utils.py
@@ -12,7 +12,7 @@ def preprocess_image(img_path):
     img = image.load_img(img_path, target_size=(224, 224))
     input_img_data = image.img_to_array(img)
     input_img_data = np.expand_dims(input_img_data, axis=0)
-    input_img_data = preprocess_input(input_img_data)  # final input shape = (1,224,224,3)
+    input_img_data = input_img_data.astype('float32') / 255.0
     return input_img_data
 
 
